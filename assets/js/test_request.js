8882bd511ac21cb4e299c7d1e160986a7f47c7c3
Wilson Nagar <41680139+WilsonNagar@users.noreply.github.com>
2021-10-02 18:21:21 +0530

    created a test request

---
+++ assets/js/test_request.js
@@ -0,0 +1,19 @@
+var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    console.log(request)
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully!");
+    })
