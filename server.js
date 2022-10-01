96f3149d53fee6172e5e14452c3461a337481581
Varshil-Shah <varshilashah09@gmail.com>
2022-10-01 12:24:37 +0530

    Refactor complete contoller file

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
 //install:
 //npm i express morgan nodemon ejs body-parser dotenv mongoose axios
 
-const express  = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const path = require('path')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const path = require("path");
 // const bodyparser = require('body-parser')
 
-const connectDB = require('./server/database/connection')
+const globalErrorHandler = require("./server/controller/error-controller");
 
-const app = express()
+const connectDB = require("./server/database/connection");
 
-dotenv.config({path:'config.env'})
+const app = express();
 
-const PORT = process.env.PORT || 8080
+dotenv.config({ path: "config.env" });
+
+const PORT = process.env.PORT || 8080;
 
 //log requests
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
 //mongodb connection
 connectDB();
 
 //parse request to body parser
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 //set view engine
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 //app.set('views',path.resolve(__dirname,"views/ejs"))
 
 //load assets
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 //load routers
-app.use('/',require('./server/routes/router'))
+app.use("/", require("./server/routes/router"));
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+// global error controller
+app.use(globalErrorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- server/model/model.js
+++ server/model/model.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 var scheme = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true 
-    },
-    gender:String,
-    status:String
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: String,
+  status: String,
+});
 
-const Userdb = mongoose.model('userdb',scheme)
-module.exports = Userdb
+const Userdb = mongoose.model("userdb", scheme);
+module.exports = Userdb;
